8323bb93b7015ba205383c97c4898c0bdab701bd
unknown <yj022011@mymail.pomona.edu>
2020-05-07 19:26:04 -0700

    Initial commit

---
+++ app/src/main/java/com/yiting/mymaps/MapsAdapter.kt
@@ -0,0 +1,38 @@
+package com.yiting.mymaps
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.yiting.mymaps.models.UserMap
+
+private const val TAG = "MapsAdapter"
+
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>, val onClickListener: OnClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    interface OnClickListener{
+        fun onItemClick(position: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount()= userMaps.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        holder.itemView.setOnClickListener {
+            Log.i(TAG, "Tapped on position $position")
+            onClickListener.onItemClick(position)
+        }
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+}
